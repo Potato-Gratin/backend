1f8296bc6ac19f75432276479b2db8eb3a87dc2a
ulxsth <ulxsth@gmail.com>
2024-11-14 04:33:57 +0900

    test: UserModel.updateByDisplayId, UserController.updateByDisplayId

--- src/tests/models/user.test.ts
+++ src/tests/models/user.test.ts
@@ -6,6 +6,7 @@ const testDisplayId = "test_display_id";
 describe("UserModel", () => {
 	beforeEach(async () => {
 		await supabase.from("user").delete().eq("display_id", testDisplayId);
+		await UserModel.create(testDisplayId, "test_name", "test_description");
 	});
 
 	afterEach(async () => {
@@ -15,14 +16,14 @@ describe("UserModel", () => {
 	describe("create", () => {
 		it("ユーザーが正常に追加されているか", async () => {
 			const user = await UserModel.create(
-				testDisplayId,
-				"test_name",
-				"test_description",
+				"new_display_id",
+				"new_name",
+				"new_description",
 			);
 			expect(user).toMatchObject({
-				display_id: testDisplayId,
-				name: "test_name",
-				description: "test_description",
+				display_id: "new_display_id",
+				name: "new_name",
+				description: "new_description",
 			});
 			expect(user).toHaveProperty("id");
 			expect(user).toHaveProperty("created_at");
@@ -30,7 +31,6 @@ describe("UserModel", () => {
 		});
 
 		it("表示IDが重複した場合は 409 conflict が返されるか", async () => {
-			await UserModel.create(testDisplayId, "test_name", "test_description");
 			await expect(
 				UserModel.create(testDisplayId, "test_name", "test_description"),
 			).rejects.toThrow("displayId is conflicted");
@@ -39,7 +39,6 @@ describe("UserModel", () => {
 
 	describe("findByDisplayId", () => {
 		it("指定した表示IDのユーザーが見つかるか", async () => {
-			await UserModel.create(testDisplayId, "test_name", "test_description");
 			const user = await UserModel.findByDisplayId(testDisplayId);
 			expect(user).toMatchObject({
 				display_id: testDisplayId,
@@ -51,6 +50,29 @@ describe("UserModel", () => {
 		it("存在しない表示IDの場合は null が返されるか", async () => {
 			const user = await UserModel.findByDisplayId("non_existent_display_id");
 			expect(user).toBeNull();
+		});
+	});
+
+	describe("updateByDisplayId", () => {
+		it("ユーザー情報が正常に更新されているか", async () => {
+			const updatedUser = await UserModel.updateByDisplayId(testDisplayId, {
+				name: "updated_name",
+				description: "updated_description",
+			});
+			expect(updatedUser).toMatchObject({
+				display_id: testDisplayId,
+				name: "updated_name",
+				description: "updated_description",
+			});
+		});
+
+		it("存在しない表示IDの場合はエラーが返されるか", async () => {
+			await expect(
+				UserModel.updateByDisplayId("non_existent_display_id", {
+					name: "updated_name",
+					description: "updated_description",
+				}),
+			).rejects.toThrow("Database Error");
 		});
 	});
 });
