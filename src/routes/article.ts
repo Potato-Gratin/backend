1f24d4ddcfb4889546a10b7264dd0250fac79924
Gingira1213 <gin12132828@icloud.com>
2024-11-15 09:56:10 +0900

    upd:ルートのariticleにarticleRouter.getを追加

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -1,3 +1,6 @@
 import express from "express";
+import { UserController } from "../controllers/user";
 
 export const articleRouter = express.Router();
+
+articleRouter.get("/users/:displayId/articles", UserController.findByDisplayId);
