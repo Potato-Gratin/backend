d0cb9c34b524711b2fc2109df90875859ea0f321
Gingira1213 <gin12132828@icloud.com>
2024-11-15 10:06:18 +0900

    fix

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import { UserController } from "../controllers/user";
 
 export const articleRouter = express.Router();
 
-articleRouter.get("/users/:displayId/articles", UserController.findByDisplayId);
