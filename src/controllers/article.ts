35ef3394332924746a62a1ce442ec4dd35c1f7e6
ulxsth <ulxsth@gmail.com>
2024-11-18 10:01:58 +0900

    chore: 記事APIをモックした

--- src/controllers/article.ts
+++ src/controllers/article.ts
@@ -2,11 +2,6 @@ import type { Request, Response } from "express";
 import { ArticleModel } from "../models/article";
 
 export const ArticleController = {
-	/**
-	 * 新しいユーザーを作成する
-	 * @param req
-	 * @param res
-	 */
 	findById: async (req: Request, res: Response) => {
 		try {
 			const { id } = req.params;
@@ -17,6 +12,61 @@ export const ArticleController = {
 				res.status(404).json({ message: "Article not found" });
 			} else {
 				res.status(200).json(article);
+			}
+		} catch (error) {
+			res.status(500).json({ message: "Internal Server Error" });
+		}
+  },
+  
+	create: async (req: Request, res: Response) => {
+		try {
+			const { title, content, user_id } = req.body;
+			const newArticle = await ArticleModel.create({ title, content, user_id });
+			res.status(201).json(newArticle);
+		} catch (error) {
+			res.status(500).json({ message: "Internal Server Error" });
+		}
+	},
+	search: async (req: Request, res: Response) => {
+		try {
+      const { q, page: pageStr = "1" } = req.query;
+
+			if (!q || typeof q !== "string") {
+        res.status(400).json({ message: "Query parameter 'q' is required" });
+        return
+      }
+      if (typeof pageStr !== "string") {
+        res.status(400).json({ message: "Query parameter 'page' must be a number" });
+        return
+      }
+
+			const articles = await ArticleModel.search(q, parseInt(pageStr));
+			res.status(200).json(articles);
+		} catch (error) {
+			res.status(500).json({ message: "Internal Server Error" });
+		}
+	},
+	update: async (req: Request, res: Response) => {
+		try {
+			const { id } = req.params;
+			const updatedArticle = await ArticleModel.updateById(id, req.body);
+			if (!updatedArticle) {
+				res.status(404).json({ message: "Article not found" });
+			} else {
+				res.status(200).json(updatedArticle);
+			}
+		} catch (error) {
+			res.status(400).json({ message: "Bad Request" });
+		}
+	},
+	delete: async (req: Request, res: Response) => {
+		try {
+			const { id } = req.params;
+			const deletedArticle = await ArticleModel.deleteById(id);
+			if (!deletedArticle) {
+				res.status(404).json({ message: "Article not found" });
+			} else {
+				res.status(200).json({ message: "Article deleted successfully" });
 			}
 		} catch (error) {
 			res.status(500).json({ message: "Internal Server Error" });
--- src/routes/article.ts
+++ src/routes/article.ts
@@ -4,3 +4,7 @@ import { ArticleController } from "../controllers/article";
 export const articleRouter = express.Router();
 
 articleRouter.get("/:id", ArticleController.findById);
+articleRouter.post("/", ArticleController.create);
+articleRouter.get("/search", ArticleController.search);
+articleRouter.patch("/:id", ArticleController.update);
+articleRouter.delete("/:id", ArticleController.delete);
