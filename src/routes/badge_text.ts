8c514c3261bdafcaa3e8d7b1e8d02f2073d3e52a
ulxsth <ulxsth@gmail.com>
2024-11-18 10:14:33 +0900

    feat: badge_textをもっくした

---
+++ src/routes/badge_text.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { BadgeTextController } from '../controllers/badge_text';
+
+const router = express.Router();
+
+router.get('/', BadgeTextController.getBadgeTexts);
+router.get('/:badgeTextId', BadgeTextController.getBadgeTextById);
+
+export default router;
