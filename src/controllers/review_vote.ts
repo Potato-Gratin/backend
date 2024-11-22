2f2b49b89bbed3449a20aa42ba1ce850bd8fc08a
Gingira1213 <gin12132828@icloud.com>
2024-11-22 10:10:25 +0900

    npm run check

--- src/controllers/review_vote.ts
+++ src/controllers/review_vote.ts
@@ -3,27 +3,31 @@ import { ReviewVoteModel } from "../models/review_vote";
 
 export const ReviewVoteController = {
 	getReviewScore: async (req: Request, res: Response) => {
-        const { reviewId, articleId } = req.params;
+		const { reviewId, articleId } = req.params;
 
-        try {
-            // モデルからスコア合計を取得
-            const totalScore = await ReviewVoteModel.getReviewScore(reviewId, articleId);
+		try {
+			// モデルからスコア合計を取得
+			const totalScore = await ReviewVoteModel.getReviewScore(
+				reviewId,
+				articleId,
+			);
 
-            if (totalScore === null) {
-                // 記事かレビューが存在しない場合
-                res.status(404).json({
-                    message: `Article with ID ${articleId} or Review with ID ${reviewId} not found.`,
-                });
+			if (totalScore === null) {
+				// 記事かレビューが存在しない場合
+				res.status(404).json({
+					message:
+						"Article with ID ${articleId} or Review with ID ${reviewId} not found.",
+				});
 				return;
-            }
+			}
 
-            res.status(200).json({ reviewId, articleId, score: totalScore });
-        } catch (error) {
-            res.status(500).json({
-                message: `Failed to fetch review score`,
-            });
-        }
-    },
+			res.status(200).json({ reviewId, articleId, score: totalScore });
+		} catch (error) {
+			res.status(500).json({
+				message: "Failed to fetch review score",
+			});
+		}
+	},
 	addOrUpdateVote: (req: Request, res: Response) => {
 		const { articleId, reviewId } = req.params;
 		const { user_id, score } = req.body;
