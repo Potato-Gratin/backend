633da610eb83112081393ec2d0a7f0e0049da813
ulxsth <ulxsth@gmail.com>
2024-12-06 12:37:03 +0900

    feat: Result 型を

--- src/controllers/review.ts
+++ src/controllers/review.ts
@@ -59,18 +59,18 @@ export const ReviewController = {
 	deleteReview: async (req: Request, res: Response) => {
 		const { articleId, reviewId } = req.params;
 
-		try {
-			// モデル層を呼び出して削除
-			const deletedReview = await ReviewModel.deleteReview(articleId, reviewId);
-
-			// データが削除されていれば成功レスポンスを返す
-			if (deletedReview && deletedReview.length > 0) {
-				res.status(200).json({ message: "Review deleted successfully." });
-			} else {
-				res.status(404).json({ message: "Review not found." });
-			}
-		} catch (error) {
-			res.status(500).json({ message: "Failed to delete review." });
+		const result = await ReviewModel.deleteReview(articleId, reviewId);
+		if (result.isFailure()) {
+			const e = result.value;
+			console.log(e);
+			res.status(500).json({ message: e.message });
 		}
+
+		const review = result.value;
+		if (!review) {
+			res.status(404).json({ message: "Review not found." });
+		}
+
+		res.status(200).json(review);
 	},
 };
