efb6c635460ed6b6b95085c7649ac706747f89a8
Gingira1213 <gin12132828@icloud.com>
2024-12-11 10:17:09 +0900

    upd:自分が受け取ったバッジ一覧取を実装する models/badgeの getReceivedBadges

--- src/models/badge.ts
+++ src/models/badge.ts
@@ -1,3 +1,18 @@
+import type { PostgrestError } from "@supabase/supabase-js";
+import supabase from "../libs/supabase";
+import { Failure, type Result, Success } from "../types/result.types";
+
+export interface Badge {
+	id: string;
+	review_id: string;
+	user_id: string;
+	created_at: string;
+	updated_at: string;
+	badge_flame_id: string;
+	badge_text_id: string;
+}
+
+
 export const BadgeModel = {
 	addBadge: (articleId: string, reviewId: number, userId: string) => {
 		return {
@@ -29,17 +44,25 @@ export const BadgeModel = {
 			},
 		];
 	},
-	getReceivedBadges: (displayId: string) => {
-		return [
-			{
-				id: "test-badge-id-1",
-				article_id: "test-article-id-1",
-				review_id: 1,
-				user_id: displayId,
-				created_at: new Date(),
-				updated_at: new Date(),
-			},
-		];
+
+	/**
+	 * 指定したユーザーIDのバッジ情報を取得する。
+	 * @param {string} userId　ユーザーID
+	 * @returns {Promise<Result<Badge | null, PostgrestError>>} 
+	 * @throws {Error} DB操作に失敗した場合
+	 */
+	getReceivedBadges: async (
+		userId: string
+	): Promise<Result<Badge | null, PostgrestError>> => {
+		const { data, error} = await supabase
+			.from("badge")
+			.select("*")
+			.eq("userId", userId);
+		if(error) {
+			return new Failure(error);
+		}
+
+		return new Success(data[0] || null);
 	},
 	getSentBadges: (displayId: string) => {
 		return [
