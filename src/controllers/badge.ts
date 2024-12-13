4f9ecf8d0392fe0481b46244160e48bf74ae93c5
Gingira1213 <gin12132828@icloud.com>
2024-12-13 10:37:23 +0900

    upd:controller/badge.tsのgetReceivedBadges

--- src/controllers/badge.ts
+++ src/controllers/badge.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from "express";
 import { BadgeModel } from "../models/badge";
+import { Result, Failure, Success } from '../types/result.types';
+import type { PostgrestError } from '@supabase/supabase-js';
 
 export const BadgeController = {
 	addBadge: (req: Request, res: Response) => {
@@ -20,10 +22,24 @@ export const BadgeController = {
 		);
 		res.status(200).json(badges);
 	},
-	getReceivedBadges: (req: Request, res: Response) => {
+
+	
+	getReceivedBadges: async (req: Request, res: Response) => {
 		const { displayId } = req.params;
-		const badges = BadgeModel.getReceivedBadges(displayId);
-		res.status(200).json(badges);
+
+		const result = await BadgeModel.getReceivedBadges(displayId);
+		if (result.isFailure()) {
+			const e = result.value;
+			console.log(e);
+			res.status(500).json({ message: e.message });
+			return;
+		}
+
+		const badge = result.value;
+		if (!badge) {
+			res.status(404).json({ message: "Badge not found" });
+		}
+		res.status(200).json(badge);
 	},
 	getSentBadges: (req: Request, res: Response) => {
 		const { displayId } = req.params;
